94f117d0ea2ffca705095050e1425e0c0869ee09
zzl <zhaoaa966@163.com>
2024-01-18 11:35:11 +0800

    update

--- src-backend/migrates/init.sql
+++ src-backend/migrates/init.sql
@@ -29,3 +29,31 @@ CREATE TABLE "public"."todos" (
   "done" bool,
   CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
 );
+
+-- create table todos mysql
+CREATE TABLE user (
+  user_id INT AUTO_INCREMENT,
+  account VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP NULL DEFAULT NULL,
+  status TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (user_id),
+  UNIQUE KEY (email)
+);
+
+-- create table todos postgres
+CREATE TABLE "public"."user" (
+  "user_id" int4 NOT NULL DEFAULT nextval('mytable_id_seq'::regclass),
+  "account" varchar(255) COLLATE "pg_catalog"."default" NOT NULL DEFAULT ''::character varying,
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL DEFAULT ''::character varying,
+  "password" varchar(255) COLLATE "pg_catalog"."default" NOT NULL DEFAULT ''::character varying,
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6),
+  "deleted_at" timestamptz(6),
+  "status" int2 NOT NULL DEFAULT 0,
+  CONSTRAINT "user_pkey" PRIMARY KEY ("user_id"),
+  CONSTRAINT "user_email_key" UNIQUE ("email")
+);
